19d86198b0fbf913fb07bf3c539a887a4a05859b
Kowshik <101036913+Venkat-Kowshik@users.noreply.github.com>
2025-02-02 10:28:29 +0530

    Working basic code with required business logic

---
+++ FAQ/translator/app/views.py
@@ -0,0 +1,22 @@
+
+# Create your views here.
+from django.shortcuts import render
+from translate import Translator
+from . import views
+from translator import views
+# Create your views here.
+
+
+def home(request):
+    if request.method == "POST":
+        text = request.POST["translate"]
+        to_lang = request.POST["tolanguage"]
+        from_lang = request.POST["fromlanguage"]
+        translator = Translator(to_lang=to_lang, from_lang=from_lang)
+        translation = translator.translate(text)
+
+        context = {
+            "translation": translation,
+        }
+        return render(request, "home.html", context)
+    return render(request, "home.html")
